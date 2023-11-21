218a42b9d999a67b3bdd433eec8b6121d54e45ff
Raymond Yeh <ray@geek.sg>
2023-11-21 17:17:52 +0800

    fix: font color for prose

--- src/components/ChatInterface/MessageBubble/MessageBubble.tsx
+++ src/components/ChatInterface/MessageBubble/MessageBubble.tsx
@@ -19,9 +19,9 @@ export const MessageBubble: FunctionComponent<{
   return (
     <div
       className={cn(
-        "flex w-max max-w-[90%] flex-col gap-2 rounded-lg px-3 py-2 text-sm overflow-x-auto prose",
+        "flex w-max max-w-[90%] flex-col gap-2 rounded-lg px-3 py-2 text-sm overflow-x-auto prose dark:prose-invert",
         message.type === "user"
-          ? "ml-auto bg-primary text-primary-foreground"
+          ? "ml-auto bg-primary text-primary-foreground prose-strong:text-primary-foreground"
           : "bg-muted"
       )}
     >
