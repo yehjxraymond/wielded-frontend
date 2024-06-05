87b86b5c289b66edf4dfd335fd00c04254dde172
Raymond Yeh <ray@geek.sg>
2024-06-05 15:17:41 +0800

    fix: lint

--- src/app/(marketing)/ai-content-mastery-course/page.tsx
+++ src/app/(marketing)/ai-content-mastery-course/page.tsx
@@ -6,50 +6,53 @@ const Content = () => {
       <div className="container">
         <div className="prose m-auto my-20">
           <h1>
-            "I'm spending more time rewriting and editing ChatGPT content than
-            if I've just done it myself!"
+            &quot;I&apos;m spending more time rewriting and editing ChatGPT
+            content than if I&apos;ve just done it myself!&quot;
           </h1>
           <p>
-            You're utterly frustrated with the low-quality, soulless content
-            that ChatGPT spits out. It feels like a waste of time trying to
-            wrestle that AI-generated mess into something usable for your
-            marketing needs. The "chatgpt voice" is so painfully generic and
-            lacks any creativity or nuance. And let's not even talk about how it
-            does nothing for SEO.
+            You&apos;re utterly frustrated with the low-quality, soulless
+            content that ChatGPT spits out. It feels like a waste of time trying
+            to wrestle that AI-generated mess into something usable for your
+            marketing needs. The &quot;chatgpt voice&quot; is so painfully
+            generic and lacks any creativity or nuance. And let&apos;s not even
+            talk about how it does nothing for SEO.
           </p>
           <p>
-            You've tried following all the "prompt engineering" advice out
-            there, but it's just not working. Your work keeps getting thrown
-            back at you, and you're at your wits' end, feeling like you're
-            failing to keep the bosses happy. The constant struggle to turn AI
-            output into something remotely passable is burning you out.
+            You&apos;ve tried following all the &quot;prompt engineering&quot;
+            advice out there, but it&apos;s just not working. Your work keeps
+            getting thrown back at you, and you&apos;re at your wits&apos; end,
+            feeling like you&apos;re failing to keep the bosses happy. The
+            constant struggle to turn AI output into something remotely passable
+            is burning you out.
           </p>
           <p>
             After spending more time trying to make it work, you are still
             thinking:
           </p>
           <blockquote>
-            It's very inorganic. There is very little to no pacing, no
+            It&apos;s very inorganic. There is very little to no pacing, no
             interesting structure. Humans write with more variety and manipulate
             tone by comparison.
           </blockquote>
           <blockquote>
-            I'm trying out the advice of LinkedIn 'gurus' and getting bad
-            results so it was either these people are full of it or it doesn't
-            work!
+            I&apos;m trying out the advice of LinkedIn &apos;gurus&apos; and
+            getting bad results so it was either these people are full of it or
+            it doesn&apos;t work!
           </blockquote>
           <blockquote>
-            I just can't get ChatGPT to write in my company's voice!!!
+            I just can&apos;t get ChatGPT to write in my company&apos;s voice!!!
           </blockquote>
           <p>
             You know there has to be a better way to leverage AI for content
-            creation that actually resonates with your audience. You've seen
-            other marketers seemingly make it work. But every time you try the
-            latest "prompt hack," you end up with more confusion and wasted
-            hours than if you'd just created the content yourself.
+            creation that actually resonates with your audience. You&apos;ve
+            seen other marketers seemingly make it work. But every time you try
+            the latest &quot;prompt hack,&quot; you end up with more confusion
+            and wasted hours than if you&apos;d just created the content
+            yourself.
           </p>
           <p>
-            Let's hold that thought for a moment and consider an alternative...
+            Let&apos;s hold that thought for a moment and consider an
+            alternative...
           </p>
           <h2>
             What if you could consistently generate high-quality,
@@ -57,97 +60,100 @@ const Content = () => {
           </h2>
           <p>
             Imagine having a simple framework for prompting ChatGPT to produce
-            focused, nuanced pieces that speak directly to your customers' pain
-            points and desires - using your brand voice.
+            focused, nuanced pieces that speak directly to your customers&apos;
+            pain points and desires - using your brand voice.
           </p>
           <p>
             Your content would capture their authentic voice and mindset,
             engaging them in a way that nurtures real relationships.
           </p>
           <p>
-            You could stop cringing, "This doesn't sound like us at all..."
-            because you'd know exactly how to infuse your brand's personality
-            into every piece of content.
+            You could stop cringing, &quot;This doesn&apos;t sound like us at
+            all...&quot; because you&apos;d know exactly how to infuse your
+            brand&apos;s personality into every piece of content.
           </p>
           <p>
-            You could stop worrying, "Will anyone point this out as written by
-            ChatGPT in public?" because you're able to shed the "ChatGPT voice"
-            and captivate your audience.
+            You could stop worrying, &quot;Will anyone point this out as written
+            by ChatGPT in public?&quot; because you&apos;re able to shed the
+            &quot;ChatGPT voice&quot; and captivate your audience.
           </p>
           <p>
             You could then repurpose that content effortlessly into multiple
-            formats tailored for different social platforms. You'd never have to
-            worry about "talking at" your audience again.
+            formats tailored for different social platforms. You&apos;d never
+            have to worry about &quot;talking at&quot; your audience again.
           </p>
           <h2>
             Learn how to use ChatGPT for content marketing confidently, by
             joining AI Content Mastery
           </h2>
           <p>
-            You'll learn step-by-step how to synthesize your existing customer
-            research, surveys, interviews, and data into detailed prompts that
-            make the AI output read like it's coming directly from the voice and
-            mindset of your target market.
+            You&apos;ll learn step-by-step how to synthesize your existing
+            customer research, surveys, interviews, and data into detailed
+            prompts that make the AI output read like it&apos;s coming directly
+            from the voice and mindset of your target market.
           </p>
           <p>
-            You'll get a framework for developing ultra-specific briefs that
-            turn ChatGPT into your empathy engine, generating audience-centric
-            ideas, outlines, and first drafts guaranteed to resonate.
+            You&apos;ll get a framework for developing ultra-specific briefs
+            that turn ChatGPT into your empathy engine, generating
+            audience-centric ideas, outlines, and first drafts guaranteed to
+            resonate.
           </p>
           <p>
-            But that's just the start. You'll also learn a repurposing workflow
-            to effortlessly transform that core content into fresh, tailored
-            formats for every social media platform and marketing channel.
+            But that&apos;s just the start. You&apos;ll also learn a repurposing
+            workflow to effortlessly transform that core content into fresh,
+            tailored formats for every social media platform and marketing
+            channel.
           </p>
           <p>
-            Want to go viral on Twitter? I'll show you how to adopt the style
-            and personality of top influencers to create tweets that get
-            attention. You'll be able to quickly spin up videos, carousels, and
-            threads that feel like they were made by the platform's biggest
-            influencer.
+            Want to go viral on Twitter? I&apos;ll show you how to adopt the
+            style and personality of top influencers to create tweets that get
+            attention. You&apos;ll be able to quickly spin up videos, carousels,
+            and threads that feel like they were made by the platform&apos;s
+            biggest influencer.
           </p>
           <p>
-            No more settling for useless "XX ChatGPT Prompts Marketers Should
-            Use" and toss that "You are an expert content marketer..." prompt
-            away.
+            No more settling for useless &quot;XX ChatGPT Prompts Marketers
+            Should Use&quot; and toss that &quot;You are an expert content
+            marketer...&quot; prompt away.
           </p>
           <p>
-            When you enroll in AI Content Mastery, here's exactly what you'll
-            do:
+            When you enroll in AI Content Mastery, here&apos;s exactly what
+            you&apos;ll do:
           </p>
           <ul>
             <li>
-              You'll learn how to train ChatGPT to sound exactly like you.
+              You&apos;ll learn how to train ChatGPT to sound exactly like you.
             </li>
             <li>
               Then learn how to choose different AI models for different needs.
             </li>
             <li>And how to get access to them for FREE (or low cost).</li>
             <li>
-              You'll build audience research materials using a unique approach.
+              You&apos;ll build audience research materials using a unique
+              approach.
             </li>
-            <li>So ChatGPT can write in your audience's language.</li>
+            <li>So ChatGPT can write in your audience&apos;s language.</li>
             <li>And understand their pain and dreams.</li>
             <li>
-              You'll plan a content calendar from the perspective of your
-              audience's life - not some lame generalities or what you feel like
-              "announcing".
+              You&apos;ll plan a content calendar from the perspective of your
+              audience&apos;s life - not some lame generalities or what you feel
+              like &quot;announcing&quot;.
             </li>
             <li>
               Then create valuable content for your audience that builds your
               reputation, earns trust, and ranks on Google.
             </li>
             <li>
-              You'll learn to distribute them across different platforms to gain
-              traffic.
+              You&apos;ll learn to distribute them across different platforms to
+              gain traffic.
             </li>
             <li>
               Like how to respond and plug your content in a Reddit thread that
-              doesn't get you banned.
+              doesn&apos;t get you banned.
             </li>
             <li>
-              And "clone" top influencers' voices to distribute content on
-              LinkedIn or Twitter if you prefer.
+              And &quot;clone&quot; top influencers&apos; voices to distribute
+              content on LinkedIn or Twitter if you prefer.
             </li>
             <li>
               You will learn how to properly prompt AI to get exactly what you
@@ -155,36 +161,38 @@ const Content = () => {
             </li>
             <li>Then build your own workflow.</li>
             <li>
-              You'll master cloning your unique approach to content marketing.
+              You&apos;ll master cloning your unique approach to content
+              marketing.
             </li>
             <li>
               Then develop prompts and workflows that replicate and automate
               your unique approach and processes.
             </li>
             <li>
-              And at the end, you'll execute your content marketing strategy
-              with a system that consistently delivers results.
+              And at the end, you&apos;ll execute your content marketing
+              strategy with a system that consistently delivers results.
             </li>
           </ul>
           <p>
-            With this system, you'll have a constant stream of creative,
+            With this system, you&apos;ll have a constant stream of creative,
             audience-obsessed marketing content that drives insane levels of
             engagement, shares, and sales.
           </p>
           <p>
-            Your posts will make your audience feel like you "get" them in a way
-            your competitors don't. And you'll learn a workflow for getting
-            ChatGPT to generate web content that actually ranks on Google -
-            using an endless trove of long tail keyword directly from your
-            audience.
+            Your posts will make your audience feel like you &quot;get&quot;
+            them in a way your competitors don&apos;t. And you&apos;ll learn a
+            workflow for getting ChatGPT to generate web content that actually
+            ranks on Google - using an endless trove of long tail keyword
+            directly from your audience.
           </p>
           <p>
-            You might be hesitant to subscribe to "yet another prompt library"
-            because you've developed an unique approach to content in your role.
+            You might be hesitant to subscribe to &quot;yet another prompt
+            library&quot; because you&apos;ve developed an unique approach to
+            content in your role.
           </p>
           <p>
-            Fret not, this course doesn't only give you prompts that works.
-            You'll learn the fundamentals of how you can develop your own
+            Fret not, this course doesn&apos;t only give you prompts that works.
+            You&apos;ll learn the fundamentals of how you can develop your own
             workflow using your unique approach to content.
           </p>
           <p>
@@ -223,21 +231,23 @@ const Content = () => {
           <p>
             Stop wasting hours trying to wrestle AI-generated nonsense into
             something usable. Enroll in the AI Content Mastery course today, and
-            you'll have everything you need to start leveraging ChatGPT's full
-            potential for audience-centric content marketing.
+            you&apos;ll have everything you need to start leveraging
+            ChatGPT&apos;s full potential for audience-centric content
+            marketing.
           </p>
           <p>
-            Just follow the step-by-step system, and you'll be publishing fresh,
-            high-converting, SEO-optimized content for every platform and
+            Just follow the step-by-step system, and you&apos;ll be publishing
+            fresh, high-converting, SEO-optimized content for every platform and
             channel in no time.
           </p>
           <h2>
             Get on our 2024 waiting list for the earliest chance to enroll
           </h2>
           <p>
-            To help the community get the maximum support and attention, we're
-            limiting the number of students in each cohort. We're currently
-            enrolling for the 2024 cohort, and spots are filling up fast.
+            To help the community get the maximum support and attention,
+            we&apos;re limiting the number of students in each cohort.
+            We&apos;re currently enrolling for the 2024 cohort, and spots are
+            filling up fast.
           </p>
           <p>
             New students are invited to enroll via our waiting list with new
